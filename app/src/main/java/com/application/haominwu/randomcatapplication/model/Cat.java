c44474381804b16aef29b29a45a0d0235e43f474
Haomin Wu <wuhaominsysu@gmail.com>
2018-08-06 21:47:01 +0800

    Remove useless import

--- app/src/main/java/com/application/haominwu/randomcatapplication/model/Cat.java
+++ app/src/main/java/com/application/haominwu/randomcatapplication/model/Cat.java
@@ -1,12 +1,6 @@
 package com.application.haominwu.randomcatapplication.model;
 
-import com.application.haominwu.randomcatapplication.callback.GetACatCallback;
-import com.application.haominwu.randomcatapplication.callback.HttpJSONCallback;
-import com.application.haominwu.randomcatapplication.util.HttpUtil;
 import com.google.gson.annotations.SerializedName;
-import com.google.gson.Gson;
-
-import org.json.JSONObject;
 
 public class Cat {
 
