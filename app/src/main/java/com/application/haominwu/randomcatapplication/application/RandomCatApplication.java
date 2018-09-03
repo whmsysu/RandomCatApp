de61d93a276c50844457bd7b2f94b9ff19b011b2
Haomin Wu <wuhaominsysu@gmail.com>
2018-09-03 09:52:26 +0800

    LeakCanary

--- app/src/main/java/com/application/haominwu/randomcatapplication/application/RandomCatApplication.java
+++ app/src/main/java/com/application/haominwu/randomcatapplication/application/RandomCatApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.blankj.utilcode.util.Utils;
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
+import com.squareup.leakcanary.LeakCanary;
 
 import org.litepal.LitePal;
 
@@ -16,5 +17,8 @@ public class RandomCatApplication extends Application {
         Logger.addLogAdapter(new AndroidLogAdapter());
         Utils.init(this);
         LitePal.initialize(this);
+        if (!LeakCanary.isInAnalyzerProcess(this)) {
+            LeakCanary.install(this);
+        }
     }
 }
