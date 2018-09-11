811a6c1dd5c6f9835e4c3d9030217ed31b90eb54
Haomin Wu <wuhaominsysu@gmail.com>
2018-09-11 11:32:22 +0800

    Add background library inject

--- app/src/main/java/com/application/haominwu/randomcatapplication/activity/BaseActivity.java
+++ app/src/main/java/com/application/haominwu/randomcatapplication/activity/BaseActivity.java
@@ -6,17 +6,20 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.application.haominwu.randomcatapplication.util.ActivityCollector;
+import com.noober.background.BackgroundLibrary;
 
 import butterknife.ButterKnife;
 
 public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        BackgroundLibrary.inject(this);
         super.onCreate(savedInstanceState);
         setContentView(getLayoutResId());
         ButterKnife.bind(this);
         init();
         ActivityCollector.pushActivity(this);
+
     }
 
     /**
